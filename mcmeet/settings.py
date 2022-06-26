91c55531a9b20e0f4830d305c5e6dd68f62d0d6f
Eniiyanu <79849737+Eniiyanu@users.noreply.github.com>
2022-06-26 22:42:37 +0100

    Update settings.py

--- mcmeet/settings.py
+++ mcmeet/settings.py
@@ -44,9 +44,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+   
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
